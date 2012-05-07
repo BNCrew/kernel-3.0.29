dbbdac7e07f0a9a68ecdb9b832e0902a747c7f0b
ukie Tux <ukie.tux@gmail.com>
2012-05-07 12:59:52 +0800

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 6 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#6 PREEMPT Sun May 6 21:52:25 CIT 2012"
+#define LINUX_COMPILE_BY "ukietux"
+#define LINUX_COMPILE_HOST "tuxkid"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.0.29-Cyanogenmod-g7f075f6-dirty"
